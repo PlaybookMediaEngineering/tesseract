6d5f8a899543f82685e25117e43b7b6e1fe932d3
yoan <yoanyombapro@gmail.com>
2024-06-10 01:30:32 -0400

    feat: add migrations
    Signed-off-by: yoan <yoanyombapro@gmail.com>

--- packages/supabase/migrations/20240609053925_schema.sql
+++ packages/supabase/migrations/20240609053925_schema.sql
@@ -18,7 +18,7 @@ CREATE TYPE transactionStatus AS ENUM ('posted', 'pending', 'excluded', 'complet
 -- Create the tables
 CREATE TABLE IF NOT EXISTS public.teams (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  created_at TIMESTAMP DEFAULT NOW(),
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   email TEXT NULL,
   inbox_email TEXT NULL,
   inbox_forwarding BOOLEAN NULL,
@@ -36,13 +36,14 @@ CREATE TABLE IF NOT EXISTS public.users (
   locale TEXT NULL,
   team_id UUID NULL,
   week_starts_on_monday BOOLEAN NULL,
-  FOREIGN KEY (team_id) REFERENCES public.teams(id)
+  CONSTRAINT users_team_id_fkey FOREIGN KEY (team_id) REFERENCES public.teams(id),
+  CONSTRAINT users_id_fkey FOREIGN KEY (id) REFERENCES public.users(id)
 );
 
 CREATE TABLE IF NOT EXISTS public.bank_connections (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   access_token TEXT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   enrollment_id TEXT NULL,
   expires_at TIMESTAMP NULL,
   institution_id TEXT NOT NULL,
@@ -50,48 +51,17 @@ CREATE TABLE IF NOT EXISTS public.bank_connections (
   name TEXT NOT NULL,
   provider bank_providers NULL,
   team_id UUID NOT NULL,
-  FOREIGN KEY (team_id) REFERENCES public.teams(id)
+  CONSTRAINT bank_connections_team_id_fkey FOREIGN KEY (team_id) REFERENCES public.teams(id)
 );
 
 SECURITY LABEL FOR pgsodium	ON COLUMN public.bank_connections.name
   IS 'ENCRYPT WITH KEY ID e11e1140-67b0-4230-968b-4293b6b23162 SECURITY INVOKER';
 
--- CREATE TABLE IF NOT EXISTS public.decrypted_bank_connections (
---   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---   access_token TEXT NULL,
---   created_at TIMESTAMP DEFAULT NOW(),
---   enrollment_id TEXT NULL,
---   expires_at TIMESTAMP NULL,
---   institution_id TEXT NOT NULL,
---   logo_url TEXT NULL,
---   name TEXT NOT NULL,
---   decrypted_name TEXT NULL,
---   provider bank_providers NULL,
---   team_id UUID NOT NULL,
---   FOREIGN KEY (team_id) REFERENCES public.teams(id)
--- );
-
--- CREATE TABLE IF NOT EXISTS public.decrypted_bank_accounts (
---   account_id TEXT NOT NULL,
---   balance NUMERIC NULL,
---   bank_connection_id UUID NULL,
---   created_at TIMESTAMP DEFAULT NOW(),
---   created_by UUID NOT NULL,
---   currency TEXT NOT NULL,
---   enabled BOOLEAN NOT NULL DEFAULT true,
---   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---   last_accessed TIMESTAMP NULL,
---   name TEXT NULL,
---   decrypted_name TEXT NULL,
---   team_id UUID NOT NULL,
---   FOREIGN KEY (team_id) REFERENCES public.teams(id)
--- );
-
 CREATE TABLE IF NOT EXISTS public.bank_accounts (
   account_id TEXT NOT NULL,
   balance NUMERIC NULL,
   bank_connection_id UUID NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   created_by UUID NOT NULL,
   currency TEXT NOT NULL,
   enabled BOOLEAN NOT NULL DEFAULT true,
@@ -99,10 +69,10 @@ CREATE TABLE IF NOT EXISTS public.bank_accounts (
   last_accessed TIMESTAMP NULL,
   name TEXT NULL,
   team_id UUID NOT NULL,
-  FOREIGN KEY (bank_connection_id) REFERENCES public.bank_connections(id),
+  CONSTRAINT bank_accounts_bank_connection_id_fkey FOREIGN KEY (bank_connection_id) REFERENCES public.bank_connections(id),
   -- FOREIGN KEY (bank_connection_id) REFERENCES public.decrypted_bank_connections(id),
-  FOREIGN KEY (created_by) REFERENCES public.users(id),
-  FOREIGN KEY (team_id) REFERENCES public.teams(id)
+  CONSTRAINT bank_accounts_created_by_fkey FOREIGN KEY (created_by) REFERENCES public.users(id),
+  CONSTRAINT public_bank_accounts_team_id_fkey FOREIGN KEY (team_id) REFERENCES public.teams(id)
 );
 
 SECURITY LABEL FOR pgsodium	ON COLUMN public.bank_accounts.name
@@ -118,7 +88,7 @@ CREATE TABLE IF NOT EXISTS public.transaction_categories (
   system BOOLEAN NULL,
   team_id UUID NOT NULL,
   vat NUMERIC NULL,
-  FOREIGN KEY (team_id) REFERENCES public.teams(id),
+  CONSTRAINT transaction_categories_team_id_fkey FOREIGN KEY (team_id) REFERENCES public.teams(id),
   UNIQUE (slug, team_id) -- Composite unique constraint
 );
 
@@ -130,7 +100,7 @@ CREATE TABLE IF NOT EXISTS public.transactions (
   bank_account_id UUID,
   category transactionCategories,
   category_slug UUID NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   currency TEXT NOT NULL,
   currency_rate NUMERIC NULL, 
   currency_source TEXT NULL,
@@ -145,7 +115,6 @@ CREATE TABLE IF NOT EXISTS public.transactions (
   team_id UUID NOT NULL,
   FOREIGN KEY (assigned_id) REFERENCES public.users(id),
   FOREIGN KEY (bank_account_id) REFERENCES public.bank_accounts(id),
-  -- FOREIGN KEY (bank_account_id) REFERENCES public.decrypted_bank_accounts(id),
   FOREIGN KEY (category_slug, team_id) REFERENCES public.transaction_categories(slug, team_id),
   FOREIGN KEY (team_id) REFERENCES public.teams(id)
 );
@@ -155,48 +124,17 @@ SECURITY LABEL FOR pgsodium	ON COLUMN public.transactions.name
 SECURITY LABEL FOR pgsodium	ON COLUMN public.transactions.description
   IS 'ENCRYPT WITH KEY ID e11e1140-67b0-4230-968b-4293b6b23162 SECURITY INVOKER';
 
--- CREATE TABLE IF NOT EXISTS public.decrypted_transactions (
---   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
---   amount NUMERIC NOT NULL,
---   assigned_id UUID NULL,
---   balance NUMERIC NULL,
---   bank_account_id UUID,
---   category transactionCategories,
---   category_slug UUID NULL,
---   created_at TIMESTAMP DEFAULT NOW(),
---   currency TEXT NOT NULL,
---   currency_rate NUMERIC NULL, 
---   currency_source TEXT NULL,
---   date TIMESTAMP DEFAULT NOW() NOT NULL,
---   description TEXT NULL,
---   decrypted_description TEXT NULL,
---   internal_id TEXT NOT NULL,
---   manual BOOLEAN NULL,
---   method transactionMethods NOT NULL,
---   name TEXT NOT NULL,
---   decrypted_name TEXT NULL,
---   note TEXT NULL,
---   status transactionStatus NULL,
---   team_id UUID NOT NULL,
---   FOREIGN KEY (assigned_id) REFERENCES public.users(id),
---   FOREIGN KEY (bank_account_id) REFERENCES public.bank_accounts(id),
---   FOREIGN KEY (bank_account_id) REFERENCES public.decrypted_bank_accounts(id),
---   FOREIGN KEY (category_slug, team_id) REFERENCES public.transaction_categories(slug, team_id),
---   FOREIGN KEY (team_id) REFERENCES public.teams(id)
--- );
-
 CREATE TABLE IF NOT EXISTS public.transaction_attachments (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  created_at TIMESTAMP DEFAULT NOW(),
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   name TEXT NULL,
   path TEXT[] NULL,
   size NUMERIC NULL,
   team_id UUID NULL,
   transaction_id UUID NULL,
   type TEXT NULL,
-  FOREIGN KEY (team_id) REFERENCES public.teams(id),
-  FOREIGN KEY (transaction_id) REFERENCES public.transactions(id),
-  -- FOREIGN KEY (transaction_id) REFERENCES public.decrypted_transactions(id)
+  CONSTRAINT public_transaction_attachments_team_id_fkey FOREIGN KEY (team_id) REFERENCES public.teams(id),
+  CONSTRAINT public_transaction_attachments_transaction_id_fkey FOREIGN KEY (transaction_id) REFERENCES public.transactions(id)
 );
 
 CREATE TABLE IF NOT EXISTS public.inbox (
@@ -204,7 +142,7 @@ CREATE TABLE IF NOT EXISTS public.inbox (
   amount NUMERIC NULL,
   attachment_id UUID NULL,
   content_type TEXT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   currency TEXT NULL,
   display_name TEXT NULL,
   due_date TEXT NULL,
@@ -221,32 +159,31 @@ CREATE TABLE IF NOT EXISTS public.inbox (
   transaction_id UUID NULL,
   website TEXT NULL,
   inbox_amount_text TEXT NULL,
-  FOREIGN KEY (attachment_id) REFERENCES public.transaction_attachments(id),
-  FOREIGN KEY (team_id) REFERENCES public.teams(id),
-  FOREIGN KEY (transaction_id) REFERENCES public.transactions(id),
-  -- FOREIGN KEY (transaction_id) REFERENCES public.decrypted_transactions(id)
+  CONSTRAINT inbox_attachment_id_fkey FOREIGN KEY (attachment_id) REFERENCES public.transaction_attachments(id),
+  CONSTRAINT public_inbox_team_id_fkey FOREIGN KEY (team_id) REFERENCES public.teams(id),
+  CONSTRAINT public_inbox_transaction_id_fkey FOREIGN KEY (transaction_id) REFERENCES public.transactions(id)
 );
 
 CREATE TABLE IF NOT EXISTS public.reports (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  created_at TIMESTAMP DEFAULT NOW(),
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   created_by UUID NULL,
   currency TEXT NULL,
   expire_at TIMESTAMP NULL,
-  from_data TIMESTAMP NULL,
+  from_date TIMESTAMP NULL,
   link_id TEXT NULL,
   short_link TEXT NULL,
   team_id UUID NULL,
   to_date TIMESTAMP NULL,
   type reportTypes,
-  FOREIGN KEY (created_by) REFERENCES public.users(id),
-  FOREIGN KEY (team_id) REFERENCES public.teams(id)
+  CONSTRAINT public_reports_created_by_fkey FOREIGN KEY (created_by) REFERENCES public.users(id),
+  CONSTRAINT reports_team_id_fkey FOREIGN KEY (team_id) REFERENCES public.teams(id)
 );
 
 CREATE TABLE IF NOT EXISTS public.tracker_projects (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   billable BOOLEAN NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   currency TEXT NULL,
   description TEXT NULL,
   estimate NUMERIC NULL,
@@ -254,29 +191,29 @@ CREATE TABLE IF NOT EXISTS public.tracker_projects (
   rate NUMERIC NULL,
   status trackerStatus,
   team_id UUID NULL,
-  project_members JSONB,
+  project_members JSONB NULL,
   total_duration NUMERIC NULL,
-  FOREIGN KEY (team_id) REFERENCES public.teams(id)
+  CONSTRAINT tracker_projects_team_id_fkey FOREIGN KEY (team_id) REFERENCES public.teams(id)
 );
 
 CREATE TABLE IF NOT EXISTS public.tracker_reports (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  created_at TIMESTAMP DEFAULT NOW(),
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   created_by UUID NULL,
   link_id TEXT NULL,
   project_id UUID NULL,
   short_link TEXT NULL,
   team_id UUID NULL,
-  FOREIGN KEY (created_by) REFERENCES public.users(id),
-  FOREIGN KEY (project_id) REFERENCES public.tracker_projects(id),
-  FOREIGN KEY (team_id) REFERENCES public.teams(id)
+  CONSTRAINT public_tracker_reports_created_by_fkey FOREIGN KEY (created_by) REFERENCES public.users(id),
+  CONSTRAINT public_tracker_reports_project_id_fkey FOREIGN KEY (project_id) REFERENCES public.tracker_projects(id),
+  CONSTRAINT tracker_reports_team_id_fkey FOREIGN KEY (team_id) REFERENCES public.teams(id)
 );
 
 CREATE TABLE IF NOT EXISTS public.tracker_entries (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   assigned_id UUID NULL,
   billed BOOLEAN NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   currency TEXT NULL,
   date TIMESTAMP NULL,
   description TEXT NULL,
@@ -286,22 +223,22 @@ CREATE TABLE IF NOT EXISTS public.tracker_entries (
   start TIMESTAMP NULL,
   stop TIMESTAMP NULL,
   team_id UUID NULL,
-  project_members JSONB,
-  FOREIGN KEY (assigned_id) REFERENCES public.users(id),
-  FOREIGN KEY (project_id) REFERENCES public.tracker_projects(id),
-  FOREIGN KEY (team_id) REFERENCES public.teams(id)
+  project_members JSONB NULL,
+  CONSTRAINT tracker_entries_assigned_id_fkey FOREIGN KEY (assigned_id) REFERENCES public.users(id),
+  CONSTRAINT tracker_entries_project_id_fkey FOREIGN KEY (project_id) REFERENCES public.tracker_projects(id),
+  CONSTRAINT tracker_entries_team_id_fkey FOREIGN KEY (team_id) REFERENCES public.teams(id)
 );
 
 
 CREATE TABLE IF NOT EXISTS public.transaction_enrichments (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   category_slug UUID NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   name TEXT NULL,
   system BOOLEAN NULL,
   team_id UUID NULL,
-  FOREIGN KEY (category_slug, team_id) REFERENCES public.transaction_categories(slug, team_id),
-  FOREIGN KEY (team_id) REFERENCES public.teams(id)
+  CONSTRAINT transaction_enrichments_category_slug_team_id_fkey FOREIGN KEY (category_slug, team_id) REFERENCES public.transaction_categories(slug, team_id),
+  CONSTRAINT transaction_enrichments_team_id_fkey FOREIGN KEY (team_id) REFERENCES public.teams(id)
 );
 
 CREATE TABLE IF NOT EXISTS public.transactions (
@@ -312,7 +249,7 @@ CREATE TABLE IF NOT EXISTS public.transactions (
   bank_account_id UUID,
   category transactionCategories,
   category_slug UUID NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   currency TEXT NOT NULL,
   currency_rate NUMERIC NULL, 
   currency_source TEXT NULL,
@@ -325,23 +262,22 @@ CREATE TABLE IF NOT EXISTS public.transactions (
   note TEXT NULL,
   status transactionStatus NULL,
   team_id UUID NOT NULL,
-  FOREIGN KEY (assigned_id) REFERENCES public.users(id),
-  FOREIGN KEY (bank_account_id) REFERENCES public.bank_accounts(id),
-  -- FOREIGN KEY (bank_account_id) REFERENCES public.decrypted_bank_accounts(id),
-  FOREIGN KEY (category_slug, team_id) REFERENCES public.transaction_categories(slug, team_id),
-  FOREIGN KEY (team_id) REFERENCES public.teams(id)
+  CONSTRAINT public_transactions_assigned_id_fkey FOREIGN KEY (assigned_id) REFERENCES public.users(id),
+  CONSTRAINT transactions_bank_account_id_fkey FOREIGN KEY (bank_account_id) REFERENCES public.bank_accounts(id),
+  CONSTRAINT transactions_category_slug_team_id_fkey FOREIGN KEY (category_slug, team_id) REFERENCES public.transaction_categories(slug, team_id),
+  CONSTRAINT public_transactions_team_id_fkey FOREIGN KEY (team_id) REFERENCES public.teams(id)
 );
 
 CREATE TABLE IF NOT EXISTS public.user_invites (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   code TEXT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
   email TEXT NULL,
   invited_by UUID NULL,
   role teamRoles NULL,
   team_id UUID NULL,
-  FOREIGN KEY (invited_by) REFERENCES public.users(id),
-  FOREIGN KEY (team_id) REFERENCES public.teams(id)
+  CONSTRAINT user_invites_invited_by_fkey FOREIGN KEY (invited_by) REFERENCES public.users(id),
+  CONSTRAINT public_user_invites_team_id_fkey FOREIGN KEY (team_id) REFERENCES public.teams(id)
 );
 
 CREATE TABLE IF NOT EXISTS public.users_on_team (
@@ -350,8 +286,8 @@ CREATE TABLE IF NOT EXISTS public.users_on_team (
   role teamRoles,
   team_id UUID,
   user_id UUID,
-  FOREIGN KEY (team_id) REFERENCES public.teams(id),
-  FOREIGN KEY (user_id) REFERENCES public.users(id)
+  CONSTRAINT users_on_team_team_id_fkey FOREIGN KEY (team_id) REFERENCES public.teams(id),
+  CONSTRAINT users_on_team_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id)
 );
 -- Create the functions
 CREATE FUNCTION public.amount_text() RETURNS TEXT AS $$
