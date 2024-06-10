63cf947491da1510272198189486dc6b0d673f5e
yoan <yoanyombapro@gmail.com>
2024-06-10 03:00:36 -0400

    feat: update nextjs config
    Signed-off-by: yoan <yoanyombapro@gmail.com>

--- apps/solomon-docs/next.config.mjs
+++ apps/solomon-docs/next.config.mjs
@@ -3,7 +3,14 @@ import nextMDX from '@next/mdx'
 import { recmaPlugins } from './src/mdx/recma.mjs'
 import { rehypePlugins } from './src/mdx/rehype.mjs'
 import { remarkPlugins } from './src/mdx/remark.mjs'
+
 import withSearch from './src/mdx/search.mjs'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Fix for __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const withMDX = nextMDX({
   options: {
@@ -21,6 +28,10 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+   webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
   },
 }
 
