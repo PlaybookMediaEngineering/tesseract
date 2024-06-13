3e84a97ad452334058d6e561cf8fe8c585ae9a66
yoan <yoanyombapro@gmail.com>
2024-06-13 00:20:20 -0400

    feat: update ui component library
    Signed-off-by: yoan <yoanyombapro@gmail.com>

--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -1,29 +1,79 @@
+import type { Preview } from '@storybook/react';
 
-import "../src/globals.css";
+import type { } from '@storybook/types';
 
-import preview from '@orbitkit/storybook/preview';
+import { withThemeByClassName } from '@storybook/addon-themes';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { type ReactRenderer } from '@storybook/react';
+
+import theme from './theme';
+
+const customViewports = {
+    '720p': {
+        name: '720p',
+        styles: {
+            width: '1280px',
+            height: '720px',
+        },
+    },
+    '1080p': {
+        name: '1080p',
+        styles: {
+            width: '1920px',
+            height: '1080px',
+        },
+    },
+    '2k': {
+        name: '2K',
+        styles: {
+            width: '2560px',
+            height: '1440px',
+        },
+    },
+    '4k': {
+        name: '4K',
+        styles: {
+            width: '3840px',
+            height: '2160px',
+        },
+    },
+    '21/9': {
+        name: '21/9',
+        styles: {
+            width: '2560px',
+            height: '1080px',
+        },
+    },
+};
+
+const preview = {
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+        viewport: {
+            viewports: {
+                ...customViewports,
+                ...INITIAL_VIEWPORTS,
+            },
+        },
+        docs: {
+            theme,
+        },
+    },
+    decorators: [
+        withThemeByClassName<ReactRenderer>({
+            themes: {
+                light: 'light',
+                dark: 'dark',
+            },
+            defaultTheme: 'dark',
+        }),
+    ],
+} satisfies Preview;
 
 export default preview;
-
-// const preview: Preview = {
-//   parameters: {
-//     actions: { argTypesRegex: "^on[A-Z].*" },
-//     controls: {
-//       matchers: {
-//         color: /(background|color)$/i,
-//         date: /Date$/,
-//       },
-//     },
-//   },
-//   decorators: [
-//     withThemeByClassName<ReactRenderer>({
-//       themes: {
-//         light: "light",
-//         dark: "dark",
-//       },
-//       defaultTheme: "light",
-//     }),
-//   ],
-// };
-
-// export default preview;
