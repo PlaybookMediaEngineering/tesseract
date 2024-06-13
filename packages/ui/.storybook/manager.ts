4f2dc0964cdd64d33187be78cf7fb0f266db73ba
yoan <yoanyombapro@gmail.com>
2024-06-13 00:18:36 -0400

    feat: update ui component library
    Signed-off-by: yoan <yoanyombapro@gmail.com>

--- packages/ui/.storybook/manager.ts
+++ packages/ui/.storybook/manager.ts
@@ -7,6 +7,37 @@
 
 import '@orbitkit/assets/fonts/stylesheet.css';
 
-import { registerAddons } from '@orbitkit/storybook/manager';
+import { useEffect } from 'react';
 
-registerAddons();
+import { addons, types, useGlobals } from '@storybook/manager-api';
+
+import theme from './theme';
+
+addons.setConfig({
+    theme: theme,
+});
+
+const ExampleToolbar = () => {
+    const [globals] = useGlobals();
+
+    useEffect(() => {
+        const elements = document.querySelectorAll('.docs-story');
+
+        elements.forEach((element) => {
+            element.classList.add(globals['theme'] as string);
+        });
+    }, [globals]);
+
+    return null;
+};
+
+export const registerAddons = () => {
+    addons.register('docs-theme', () => {
+        addons.add('docs-theme-addon', {
+            title: 'Addon to change docs story theme',
+            type: types.TOOL,
+            match: ({ viewMode }) => !!viewMode?.match(/^(?:story|docs)$/),
+            render: ExampleToolbar,
+        });
+    });
+};
